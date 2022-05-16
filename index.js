c9f5cc6d284f5cfea0ff80ee334d20f48c3455d7
kgp82 <grandpierre.kevin@gmail.com>
2022-05-16 13:20:00 +0200

    test js

---
+++ index.js
@@ -0,0 +1,10 @@
+// concaténation string
+let myName = 'Grandpierre';
+let firstName = 'Kévin';
+let completName = myName +' '+firstName;
+console.log (completName);
+let letter = myName[0]
+console.log(letter);
+let longueur = myName.length
+console.log(longueur);
+console.log(myName.toUpperCase());
