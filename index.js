1248f0db0234ebbbb043a756a7677d94e6441af3
kgp82 <grandpierre.kevin@gmail.com>
2022-05-16 22:14:25 +0200

    fin de journée

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ console.log(letter);
 let longueur = myName.length
 console.log(longueur);
 console.log(myName.toUpperCase());
+let fruits =['bananes', 'pomme', 'pamplemousse', 'mangue', 'cerise']
+fruits [3] = 'ananas'
+console.log(fruits.length);
+console.log(fruits.indexOf('ananas'))
+let email = 'grandpierre.kevin@gmail.com'
+console.log(email.includes('&'));
+let myAge = 30
+console.log(myAge >= 20);
+
+// boucle
+for(let i= 20; i<100; i++)
+    console.log('boucle numéro' + i);
+
